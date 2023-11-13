8d28fbd1c9e0f2d6134216049a3553c640380809
Tarek <tarekalzaza@outlook.com>
2023-11-13 09:38:02 +0000

    changes

--- ProducerService/ProducerService.js
+++ ProducerService/ProducerService.js
@@ -5,44 +5,41 @@ const Web3 = require("web3");
 const contractABI = require("../SmartContract/abi.json");
 require("dotenv").config();
 
+// Create an express app, and use bodyParser to parse input of CDRs in JSON
 const app = express();
-
-// Use body-parser to parse input of CDRs in JSON
 app.use(bodyParser.json());
 
-// Connect to Ethereum node
+// Create an instance of Web3 with an HttpProvider
 const web3 = new Web3.Web3(
-  new Web3.providers.http.HttpProvider(
-    "https://goerli.infura.io/v3/ad452e395e9341d8808a7411e59ebcad"
-  )
+  new Web3.providers.http.HttpProvider(process.env.HTTP_PROVIDER_URL)
 );
 
-const contractAddress = "0x26fcDA85fAB8925E0BB9eA90eEE400734dc91FAf";
-
 // Initialize the contract
+const contractAddress = process.env.CONTRACT_ADDRESS;
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-// Private key of the account that will send transactions
+// Private key of the account to sign transactions sent to the blockchain
 const privateKey = process.env.PRIVATE_KEY;
 
-// Hash CDR function
+// Function that converts the CDR object to a string and hashes it using SHA256
 const hashCdr = (cdr) => {
   const cdrString = `${cdr.Id},${cdr.Date},${cdr.Time},${cdr.MSISDN},${cdr.CalledParty},${cdr.CallDuration},${cdr.ChargeDuration},${cdr.CellId},${cdr.CalledCellId}`;
   return crypto.createHash("sha256").update(cdrString).digest("hex");
 };
 
-// Express route for receiving a POST request with CDR data
+// Endpoint for receiving POST requests with CDR data
 app.post("/cdr", async (req, res) => {
   try {
     const cdr = req.body;
     const hashValue = hashCdr(cdr);
-    console.log("Resulting hash: ", hashValue);
-    // The account address derived from the private key
+
+    // Get the public account address to send in the transaction (derived from the private key)
     const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 
     // Get current gas price to send with the transaction
     const gasPrice = await web3.eth.getGasPrice();
-    // Prepare the transaction
+
+    // Prepare the transaction object
     const tx = {
       from: account.address,
       to: contractAddress,
@@ -54,7 +51,7 @@ app.post("/cdr", async (req, res) => {
     // Sign the transaction with the private key
     const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
 
-    // Send the signed transaction
+    // Send the signed transaction to the blockchain
     const receipt = await web3.eth.sendSignedTransaction(
       signedTx.rawTransaction
     );
@@ -62,7 +59,10 @@ app.post("/cdr", async (req, res) => {
     // Respond with the transaction receipt
     res
       .status(200)
-      .json({ message: "CDR inserted successfully", txHash: receipt.transactionHash.toString() });
+      .json({
+        message: "CDR inserted successfully",
+        txHash: receipt.transactionHash.toString(),
+      });
   } catch (error) {
     console.error("Error inserting CDR:", error);
     res
