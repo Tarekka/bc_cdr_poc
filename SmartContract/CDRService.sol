62800389ef243fe6548bdf8392211c4a1ded9a89
Tarek <tarekalzaza@outlook.com>
2023-11-18 16:43:37 +0000

    changes

--- SmartContract/CDRService.sol
+++ SmartContract/CDRService.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract CdrContract {
-    // Struct to hold CDR data along with a timestamp and the sender's address
+    // Struct to hold hash value of a CDR with the current timestamp
     struct CdrData {
         string hashValue;
         uint256 timestamp;
     }
 
-    // Dictionary that stores CDRs, with their unique Ids as keys
+    // Nested dictionary that stores a dictionary for each producer, that stores their CDRs with their unique Ids as keys
     mapping(address => mapping(uint256 => CdrData)) private cdrs;
 
     // Event to emit when a new CDR is inserted
@@ -27,7 +27,7 @@ contract CdrContract {
     }
 
     // Function to view a CDR and its details - used by the verification service
-    function viewCdr(address originator, uint256 uniqueId) public view returns (string memory, uint256) {
+    function viewCdr(address originator, uint256 uniqueId) public view returns (string memory hashValue, uint256 timestamp) {
         require(bytes(cdrs[originator][uniqueId].hashValue).length > 0, "CDR does not exist.");
 
         CdrData memory cdrData = cdrs[originator][uniqueId];
--- VerificationService/VerificationService.js
+++ VerificationService/VerificationService.js
@@ -17,6 +17,7 @@ const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 // Define types of results that can be returned by the VerifyCdr function
 const Results = {
+  NotFound: "NotFound",
   HashMismatch: "HashMismatch",
   TimeMismatch: "TimeMismatch",
   Valid: "Valid",
@@ -24,37 +25,38 @@ const Results = {
 };
 
 // Get CDR data from contract and verify with the record passed in
-async function VerifyCdr(cdr) {
+async function VerifyCdr(address, cdr) {
   try {
     // Get the record from the blockchain
-    const result = await contract.methods
-      .viewCdr("0x143CE9C93e5F03e860FFEeDEf9D97F621104ad70", cdr.Id)
-      .call();
+    const result = await contract.methods.viewCdr(address, cdr.Id).call();
 
     // Prepare local data of the record for verification with the blockchain result
+    // 1- Convert the CDR date and time to unix timestamp for comparision
     let record_Date = new Date(cdr.Date + "T" + cdr.Time).getTime() / 1000;
+    // 2- Hash the CDR for comparision
     var record_Hash = hashCdr(cdr);
 
-    console.log(Number(result[1]))
-    console.log(record_Date);
     // Check if the hash in the blockchain matches our hash
     if (result[0] != record_Hash) {
       return `${cdr.Id},${Results.HashMismatch}`;
     }
     // Check if the time the record was entered to the blockchain matches the record's date.
-    else if (Math.abs(record_Date - Number(result[1])) > (300 * 1000)) {
+    else if (Math.abs(record_Date - Number(result[1])) > 10 * 1000) {
       return `${cdr.Id},${Results.TimeMismatch}`;
     }
     // If it passed both checks, then the record is valid
     return `${cdr.Id},${Results.Valid}`;
-  } catch (error) {;
-    // Print innerError if available, else print the outer error message. 
+  } catch (error) {
+    // Print innerError if available, else print the outer error message.
     if (error.innerError) {
-      console.error("Inner error details:", error.innerError);
-    } else {
-      console.error("An error occurred:", error.message)
+      if (error.innerError.message == "execution reverted: CDR does not exist.")
+        return `${cdr.Id},${Results.NotFound}`;
+      console.error("Inner error details:", error.innerError.message);
+    } 
+    else {
+      console.error("An error occurred:", error.message);
     }
-    return `${cdr.Id},${Results.Error}`;
+    return `${cdr.Id},${Results.Error}`; // 
   }
 }
 
@@ -65,13 +67,13 @@ const hashCdr = (cdr) => {
 };
 
 // Function to read CSV file and process each line
-function processCSV(filePath) {
+function processCSV(filePath, address) {
   return new Promise((resolve, reject) => {
     const results = [];
     fs.createReadStream(filePath)
       .pipe(csv())
       .on("data", (row) => {
-        results.push(VerifyCdr(row));
+        results.push(VerifyCdr(address, row));
       })
       .on("end", () => {
         Promise.all(results)
@@ -86,23 +88,25 @@ function processCSV(filePath) {
   });
 }
 
-// Read file path from user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.question("Enter the path of your CSV file: ", async (filePath) => {
-  await processCSV(filePath);
-  rl.close();
-});
-
 // Function that write the results to a new file
 function writeToFile(data, fileName) {
   const content = data.join("\n");
   fs.writeFile(fileName, content, (err) => {
     if (err) {
-      return console.error(`Error occured while writing results to file: ${err.message}`);
+      return console.error(
+        `Error occured while writing results to file: ${err.message}`
+      );
     }
     console.log(`Results written to ${fileName}`);
   });
 }
+
+// Access command line arguments
+const args = process.argv.slice(2);
+if (args.length != 2) {
+  console.error(
+    "Please pass in the input file path and the address of the producer.\nExample input.csv 0x123456"
+  );
+  return;
+}
+processCSV(args[0], args[1]);
